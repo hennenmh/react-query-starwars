85b473139e79e4dd473719b33a4b08b9f277dbde
TMXH4 <mark_hennen@unigroup.com>
2022-03-16 14:24:23 -0500

    Added fetch to SWAPI and now returning basic planets and people data

--- src/components/People.js
+++ src/components/People.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
+import Person from './Person';
 
-const  People = () => {
-  return (
-      <div>
-        <h2>People</h2>
-      </div>
-  );
+const queryClient = new QueryClient();
+const fetchPeople = async () => {
+  const res = await fetch('http://swapi.dev/api/people/');
+  return res.json();
 }
 
-export default People;
+const People = () => {
+  const { data, status } = useQuery('people', fetchPeople);
+  console.log(data);
+
+  return (
+    <div>
+      <h2>People</h2>
+      {status === 'loading' && (
+        <div>Loading data...</div>
+      )}
+      {status === 'error' && (
+        <div>Error fetching data</div>
+      )}
+      {status === 'success' && (
+        <div>
+          {data.results.map(person => <Person key={person.name} person={person}/> )}
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default function Wrapped() {
+  return (<QueryClientProvider client={queryClient}>
+    <People/>
+  </QueryClientProvider>)
+};
--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
+import Planet from './Planet';
+
+const queryClient = new QueryClient();
+const fetchPlanets = async () => {
+  const res = await fetch('http://swapi.dev/api/planets/');
+  return res.json();
+}
 
 const Planets = () => {
+  const { data, status } = useQuery('planets', fetchPlanets);
+  console.log(data);
+
   return (
       <div>
         <h2>Planets</h2>
+        {status === 'loading' && (
+            <div>Loading data...</div>
+        )}
+        {status === 'error' && (
+            <div>Error fetching data</div>
+        )}
+        {status === 'success' && (
+            <div>
+              {data.results.map(planet => <Planet key={planet.name} planet={planet}/> )}
+            </div>
+        )}
       </div>
   )
 }
 
-export default Planets;
+export default function Wrapped() {
+  return (<QueryClientProvider client={queryClient}>
+    <Planets/>
+  </QueryClientProvider>)
+};
